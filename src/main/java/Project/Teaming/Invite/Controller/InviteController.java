aeecce263b20986ab7ea7f8c2f0dd96e77850229
김연호 <kimfine000@gmail.com>
2025-07-09 09:53:26 +0900

    import 최적화

--- src/main/java/Project/Teaming/Invite/Controller/InviteController.java
+++ src/main/java/Project/Teaming/Invite/Controller/InviteController.java
@@ -4,7 +4,6 @@ import Project.Teaming.Invite.Dto.InviteRequestDto;
 import Project.Teaming.Invite.Service.InviteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/Project/Teaming/Invite/Service/InviteService.java
+++ src/main/java/Project/Teaming/Invite/Service/InviteService.java
@@ -2,8 +2,6 @@ package Project.Teaming.Invite.Service;
 
 import Project.Teaming.Invite.Dto.InviteRequestDto;
 
-import java.security.Principal;
-
 public interface InviteService {
     void sendInvite(String managerUsername, InviteRequestDto dto);
 }
