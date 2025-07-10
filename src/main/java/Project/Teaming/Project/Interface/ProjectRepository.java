47209963d19003ad989d0129fdcb4dea9335e469
김연호 <kimfine000@gmail.com>
2025-07-10 23:28:19 +0900

    delete Member, Project package

--- src/main/java/Project/Teaming/Project/Interface/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package Project.Teaming.Project.Interface;
-
-import Project.Teaming.Project.Entity.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface ProjectRepository extends JpaRepository<Project, Integer> {
-}
