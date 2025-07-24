07931b9e1488b928785913821be5fa47d32c820f
김연호 <kimfine000@gmail.com>
2025-07-24 14:41:03 +0900

    chore: 쓸모 없는 어노테이션 삭제

--- src/main/java/project/teaming/invite/service/InviteService.java
+++ src/main/java/project/teaming/invite/service/InviteService.java
@@ -9,7 +9,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.List;
 
 public interface InviteService {
-    void sendInvite(@AuthenticationPrincipal UserDetails userDetails, InviteRequestDto dto);
+    void sendInvite(UserDetails userDetails, InviteRequestDto dto);
     void acceptInvite(UserDetails userDetails, AcceptInviteRequestDto dto);
     void refuseInvite(UserDetails userDetails, AcceptInviteRequestDto dto);
     List<Invite> getAllInvites();
